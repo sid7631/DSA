72116de27eb88c96e74382921fc898c49d8febe6
Siddharth Sarkar <sidsarkar07@gmail.com>
2023-03-29 18:08:31 +0000

    added main

--- two_pointers/sum_of_three.py
+++ two_pointers/sum_of_three.py
@@ -21,3 +21,24 @@ def find_sum_of_three(nums, target):
             else:
                 high -= 1
     return False
+
+def main():
+    """main method"""
+    nums_lists = [[3, 7, 1, 2, 8, 4, 5],
+                  [-1, 2, 1, -4, 5, -3],
+                  [2, 3, 4, 1, 7, 9],
+                  [1, -1, 0],
+                  [2, 4, 2, 7, 6, 3, 1]]
+
+    targets = [10, 7, 20, -1, 8]
+
+    for i in range(len(nums_lists)):
+        print(i + 1, ".\tInput array: ", nums_lists[i], sep="")
+        if find_sum_of_three(nums_lists[i], targets[i]):
+            print("\tSum for", targets[i], "exists")
+        else:
+            print("\tSum for", targets[i], "does not exist")
+        print("-"*100)
+
+if __name__ == '__main__':
+    main()
